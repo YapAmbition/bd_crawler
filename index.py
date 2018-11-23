ec59c95395b820055c9975d0c1dfa3e0fc8c215a
沈真成 <zhencheng.shen02@ele.me>
2018-11-23 11:34:32 +0800

    添加readme

--- index.py
+++ index.py
@@ -7,9 +7,10 @@ import download
 
 
 img_dir = 'imgs/'
+key = '刀塔'
 
 # 查询内容
-content = urllib.quote('刀塔')
+content = urllib.quote(key)
 # 查询第几页
 page = 1
 url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&gsm=78&ct=&ic=0&lm=-1&width=0&height=0&pn=%d&word=%s' % (page, content)
